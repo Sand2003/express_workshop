5c8ac9964fbd27b4a4e8dc7e3f077ccd8eb697e0
Sandra_Fermin <sandy.paola2003@gmail.com>
2021-09-06 17:57:09 -0500

    added pokemon POST

---
+++ config/database.js
@@ -0,0 +1,16 @@
+const mysql = require("mysql");
+const util = require('util');
+
+
+const pool = mysql.createPool({
+    conectionlimit: 10,
+    host: 'localhost',
+    user: 'root',
+    pasword: '',
+    database: 'pokemon'
+});
+
+
+pool.query = util.promisify(pool.query);
+
+module.exports = pool;
--- index.js
+++ index.js
@@ -1,12 +1,26 @@
+const morgan = require('morgan');
 const express = require("express");
 const app = express();
+const pokemon = require('./routes/pokemon');
+
+
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended : true}))
 
 
 app.get("/", (req, res, next) => {
-res.status(200);
-res.send("Bienvenido");
+return res.status(200).json({code: 1, message: "Bienvenido al Pokedex"});
 });
 
-app.listen(3000, () => {
- console.log("server is running...");
+
+app.use("/pokemon", pokemon);
+
+app.use((req,res,next) => {
+    return res.status(404).json({code: 404, message: "URL no encontrado"});
+});
+
+
+app.listen(process.env.PORT || 3000, () => {
+ console.log("Server is running...");
 });
